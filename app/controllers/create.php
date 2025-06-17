7b8ddd0d24af2f9a19c992b1abcb1b4201bb130d
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-06-17 03:37:10 +0000

    Update Create Controller

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -2,7 +2,36 @@
 
 class Create extends Controller {
 
-    public function index() {		
-	    $this->view('create/index');
+    public function index() {
+        $message = "";
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $username = trim($_POST["username"]);
+            $password = $_POST["password"];
+            $confirm_password = $_POST["confirm_password"];
+
+            if (empty($username) || empty($password) || empty($confirm_password)) {
+                $message = "All fields are required.";
+            } elseif (strlen($password) < 10) {
+                $message = "Password must be at least 10 characters long.";
+            } elseif ($password !== $confirm_password) {
+                $message = "Passwords do not match.";
+            } else {
+                try {
+                    require_once 'app/models/User.php';
+                    $user = new User();
+                    if ($user->user_exists($username)) {
+                        $message = "<span style='color:red;'>Account cannot be created. Username already exists.</span>";
+                    } else {
+                        $user_id = $user->create_user($username, $password);
+                        $message = "Account created successfully. Your user ID is $user_id.<br><a href='/login'>Click here to login</a>";
+                    }
+                } catch (Exception $e) {
+                    $message = "Error: " . $e->getMessage();
+                }
+            }
+        }
+
+        $this->view('create/index', ['message' => $message]);
     }
 }
