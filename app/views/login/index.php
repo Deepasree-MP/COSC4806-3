c1122f604275ca0b79cbacc2bcf7ba8404b8d18a
deepasreemp <dmeenapadmanabhan@algomau.ca>
2025-06-18 06:50:22 -0400

    Back to Working Model

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,31 +1,29 @@
-	<?php require_once "app/views/templates/headerPublic.php"; ?>
-	<main role="main" class="container">
-			<div class="page-header" id="banner">
-					<div class="row">
-							<div class="col-lg-12">
-									<h1>You are not logged in</h1>
-							</div>
-					</div>
-			</div>
+<?php require_once 'app/views/templates/headerPublic.php'?>
+<main role="main" class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>You are not logged in</h1>
+            </div>
+        </div>
+    </div>
 
-	<div class="row">
-			<div class="col-sm-auto">
-			<form action="/login/verify" method="post" >
-			<fieldset>
-				<div class="form-group">
-					<label for="username">Username</label>
-					<input required type="text" class="form-control" name="username">
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<input required type="password" class="form-control" name="password">
-				</div>
-							<br>
-					<button type="submit" class="btn btn-primary">Login</button>
-				<br>
-				<a href="/signup" class="btn btn-secondary mt-2">Sign Up</a>
-			</fieldset>
-			</form> 
-		</div>
+<div class="row">
+    <div class="col-sm-auto">
+		<form action="/login/verify" method="post" >
+		<fieldset>
+			<div class="form-group">
+				<label for="username">Username</label>
+				<input required type="text" class="form-control" name="username">
+			</div>
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input required type="password" class="form-control" name="password">
+			</div>
+            <br>
+		    <button type="submit" class="btn btn-primary">Login</button>
+		</fieldset>
+		</form> 
 	</div>
-			<?php require_once "app/views/templates/footer.php"; ?>
+</div>
+    <?php require_once 'app/views/templates/footer.php' ?>
