f4e40351a2cf8a36b9c8ce63912b9e40ea3c196b
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-06-19 03:29:37 +0000

    Validate using Pearl CSS

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,75 +1,107 @@
-<?php
-if (!isset($_SESSION['auth'])) {
-    header('Location: /login');
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/favicon.png">
-    <title>COSC 4806</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-  </head>
-  <body>
-    <!-- User Skeleton Navbar -->
-    <nav>
-      <div class="container">
-        <div class="row">
-          <div class="twelve columns">
-            <h4 style="margin-bottom: 0;"><a href="/">COSC 4806</a></h4>
-            <ul style="list-style: none; padding: 0; margin-top: 10px;">
-              <li style="display: inline-block; margin-right: 15px;"><a href="/home">Home</a></li>
-              <li style="display: inline-block; margin-right: 15px;"><a href="/about">About Me</a></li>
-              <li style="display: inline-block; margin-right: 15px;"><a href="#">Dropdown</a></li>
-              <li style="display: inline-block; margin-right: 15px;"><a href="#" class="disabled">Disabled</a></li>
+    <?php
+    if (!isset($_SESSION['auth'])) {
+        header('Location: /login');
+    }
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <!-- Bootstrap CDN -->
+        <!--
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+              crossorigin="anonymous">
+        -->
+
+        <!-- Skeleton CDN -->
+        <!--
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+        -->
+
+        <!-- Pearl CSS CDN (Active) -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pearlcss@1.0.2/dist/pearl.min.css">
+
+        <!-- Viewport & Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+        <link rel="icon" href="/favicon.png">
+        <title>COSC 4806</title>
+      </head>
+      <body>
+
+        <!-- Navbar for Skeleton CSS -->
+        <!--
+        <nav>
+          <div class="container">
+            <div class="row">
+              <div class="twelve columns">
+                <h4 style="margin-bottom: 0;"><a href="/">COSC 4806</a></h4>
+                <ul style="list-style: none; padding: 0; margin-top: 10px;">
+                  <li style="display: inline-block; margin-right: 15px;"><a href="/home">Home</a></li>
+                  <li style="display: inline-block; margin-right: 15px;"><a href="/about">About Me</a></li>
+                  <li style="display: inline-block; margin-right: 15px;"><a href="#">Dropdown</a></li>
+                  <li style="display: inline-block; margin-right: 15px;"><a href="#" class="disabled">Disabled</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+        -->
+
+        <!-- Navbar for Pearl CSS (Active) -->
+        <nav class="nav p-2 border-bottom bg-light">
+          <div class="container flex justify-between items-center">
+            <h4><a href="/" class="nav-brand text-bold">COSC 4806</a></h4>
+            <ul class="flex gap-3 list-none m-0">
+              <li><a href="/home" class="link">Home</a></li>
+              <li><a href="/about" class="link">About Me</a></li>
+              <li><a href="#" class="link">Dropdown</a></li>
+              <li><a href="#" class="link disabled">Disabled</a></li>
             </ul>
           </div>
-        </div>
-      </div>
-    </nav>
+        </nav>
 
-    <!-- User bootstrap Navbar -->
-    <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">COSC 4806</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about">About Me</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Dropdown </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">Action</a>
+        <!-- Navbar for Bootstrap -->
+        <!--
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">COSC 4806</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="/home">Home</a>
                 </li>
-                <li>
-                  <a class="dropdown-item" href="#">Another action</a>
+                <li class="nav-item">
+                  <a class="nav-link" href="/about">About Me</a>
                 </li>
-                <li>
-                  <hr class="dropdown-divider">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                     data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
                 </li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
                 </li>
               </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>-->
+            </div>
+          </div>
+        </nav>
+        -->
--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,19 +1,35 @@
-<?php
-if (isset($_SESSION['auth']) == 1) {
-    header('Location: /home');
-}
-?>
+    <?php
+    if (isset($_SESSION['auth']) == 1) {
+        header('Location: /home');
+    }
+    ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="/favicon.png">
-    <title>COSC 4806</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-</head>
-<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <!-- Bootstrap CDN -->
+        <!--
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+              rel="stylesheet" 
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+              crossorigin="anonymous">
+        -->
+
+        <!-- Skeleton CDN -->
+        <!--
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css">
+        -->
+
+        <!-- Pearl CSS CDN (Active) -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pearlcss@1.0.2/dist/pearl.min.css">
+
+        <!-- Viewport (needed for all frameworks) -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" href="/favicon.png">
+        <title>COSC 4806</title>
+        <meta charset="UTF-8">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+    </head>
+    <body>
