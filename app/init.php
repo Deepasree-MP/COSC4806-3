cf70ce7f9f11b1a0add451cdd35bdf94c2caa9b6
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-06-19 04:43:11 +0000

    reset back to 8 hrs

--- app/init.php
+++ app/init.php
@@ -4,8 +4,8 @@ error_reporting(0);
 ini_set('session.gc_maxlifetime', 28800);
 ini_set('session.gc_probability', 1);
 ini_set('session.gc_divisor', 1);
-//$sessionCookieExpireTime = 28800; // 8hrs
-$sessionCookieExpireTime = 10; // 10 seconds
+$sessionCookieExpireTime = 28800; // 8hrs
+//$sessionCookieExpireTime = 10; // 10 seconds
 session_set_cookie_params($sessionCookieExpireTime);
 session_start();
 
