b9e3f584c6da7b30f425b9ef85b60d3004c1da8c
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-06-19 04:27:37 +0000

    Add New CSS styles

--- app/views/login/index.php
+++ app/views/login/index.php
@@ -1,38 +1,38 @@
-	<?php require_once 'app/views/templates/headerPublic.php' ?>
-	<main role="main" class="container">
-			<div class="page-header" id="banner">
-					<div class="row">
-							<div class="col-lg-12">
-									<h1>You are not logged in</h1>
-							</div>
-					</div>
-			</div>
+<?php require_once 'app/views/templates/headerPublic.php' ?> 
 
-			<div class="row">
-					<div class="col-sm-auto">
-						<?php if (isset($_SESSION['error_message'])): ?>
-								<div class="alert alert-danger">
-										<?= htmlspecialchars($_SESSION['error_message']) ?>
-								</div>
-								<?php unset($_SESSION['error_message']); ?>
-						<?php endif; ?>
-							<form action="/login/verify" method="post">
-									<fieldset>
-											<div class="form-group">
-													<label for="username">Username</label>
-													<input required type="text" class="form-control" name="username">
-											</div>
-											<div class="form-group">
-													<label for="password">Password</label>
-													<input required type="password" class="form-control" name="password">
-											</div>
-											<br>
-											<button type="submit" class="btn btn-primary">Login</button>
-											<br><br>
-										<a href="/create" class="btn btn-secondary">Create Account</a>
-									</fieldset>
-							</form> 
-					</div>
+<main class="container d-flex align-items-center justify-content-center" style="min-height: 90vh;">
+	<div class="col-md-6 col-lg-5">
+		<div class="card shadow">
+			<div class="text-center mt-3 mb-4">
+				<h2 class="fw-normal">You are not logged in</h2>
 			</div>
-	</main>
-	<?php require_once 'app/views/templates/footer.php' ?>
+			<div class="card-body">
+
+				<?php if (isset($_SESSION['error_message'])): ?>
+					<div class="alert alert-danger">
+						<?= htmlspecialchars($_SESSION['error_message']) ?>
+					</div>
+					<?php unset($_SESSION['error_message']); ?>
+				<?php endif; ?>
+
+				<form action="/login/verify" method="post">
+					<div class="mb-3">
+						<label for="username" class="form-label">Username</label>
+						<input required type="text" class="form-control" id="username" name="username">
+					</div>
+
+					<div class="mb-3">
+						<label for="password" class="form-label">Password</label>
+						<input required type="password" class="form-control" id="password" name="password">
+					</div>
+
+					<div class="d-grid gap-2">
+						<button type="submit" class="btn btn-danger">Login</button>
+						<a href="/create" class="btn btn-outline-primary">Create an Account</a>
+					</div>
+				</form>
+
+			</div>
+		</div>
+	</div>
+</main>
