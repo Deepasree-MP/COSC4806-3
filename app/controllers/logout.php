65bdf1cb1788e0639c9064abba1483902d45c787
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-06-19 02:40:03 +0000

    add session_start as required

--- app/controllers/logout.php
+++ app/controllers/logout.php
@@ -5,6 +5,11 @@ class Logout extends Controller {
     public function index() {		
 	    session_start();
         $_SESSION = array();
+        /*echo '<h3>Session contents before clearing:</h3>';
+        echo '<pre>';
+        print_r($_SESSION);
+        echo '</pre>';
+        die;*/
         session_destroy();
         header('location:/login');
     }
